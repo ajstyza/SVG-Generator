d3ae3154c0c586e87d0b023ac566e59cdd1af80f
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-06-01 15:31:51 -0700

    added promises

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const { readFile, writeFile } = require('fs/promises');
 const fs = require('fs');
 const Triangle = require ('./lib/shapes.js');
 const Square = require('./lib/shapes.js');
@@ -21,37 +22,36 @@ const questions = [{
 }];
 
 function init() {
-    inquirer.prompt(questions)
+    return inquirer.prompt(questions)
     .then((data) => {
-        console.log('generated logo.svg', data);
-        writeToFile(data);
-        let shape;
+        
+        // writeToFile(data);
+        let shape = JSON.parse(data);
         
         if(data.shape[0] == 'Square'){
-            shape = new Square('Square')
-            shape.prototype.add('Square')
+            shape = new Square()
+            
         }
         else if(data.shape[1] == 'Circle') {
         shape = new Circle() 
-        shape.prototype.add('Circle')
         }
         
         else(data.shape[2] == 'Triangle') 
-            shape = new Triangle()
-            shape.prototype.add('Triangle');
+            shape = new Triangle()        
     })
-        .then((shape) => {
-        let textColor;
-            if(data.textColor == 'green' || '#00FF00'){ // is this how to add text color?
-                shape.prototype.add('green');
-            }
-        })
+    .then((shape) => {
+        return writeFile('logo.svg', shape)
+    })
+    .then(() => {
+        console.log('generated logo.svg');
+    })
+            
+
+        // })
         // writeToFile(data);     
         // console.log(shape);
 // function writeToFile(shape) {
 //     fs.writeFile("logo.svg", generateSvg(shape, null, '\t'), (err) =>
 //         err ? console.log(err) : console.log('generated logo.svg'))   
-}
-;
-
+};
 init();
